fb757b8301be4a542cbabd1254e6608c797e220e
hikozuma <curoas@yahoo.co.jp>
2018-05-01 10:57:14 +0000

    fix typo

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  # get 'welcome/home', to: 'welcome#home'
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
 end
