41f4785709aa6dd7d9855dc1c30919873b0091c3
hikozuma <curoas@yahoo.co.jp>
2018-05-16 04:09:46 +0000

    reset alpha-blog for relearning

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_02_062936) do
+ActiveRecord::Schema.define(version: 2018_05_16_035845) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
