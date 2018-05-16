2766bc633042f0bd3bbe6b2875252b74b1da9491
hikozuma <curoas@yahoo.co.jp>
2018-05-16 06:17:13 +0000

    fix create, add flash and error messages, create show

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -6,10 +6,27 @@ class ArticlesController < ApplicationController
   def create
     # check passing values
     # render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.save
+    # @article = Article.new(article_params)
+    # @article.save
     
-    redirect_to articles_show(@article)
+    # redirect_to articles_show(@article)
+    # redirect_to article_path(@article)
+    
+    @article = Article.new(article_params)
+    
+    if @article.save
+      # this message will be disappear when reload
+      flash[:notice] = "Article was success fully created"
+      redirect_to article_path(@article)
+      
+    else
+      render 'new'
+      
+    end 
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
    
   private
