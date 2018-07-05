987491397dac5044247c2e1f84404c34f124579f
hikozuma <curoas@yahoo.co.jp>
2018-07-05 06:38:20 +0000

    add edit update functionality and partical

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-   def new
-       @user = User.new
-   end 
+    def new
+        @user = User.new
+    end 
    
-   def create
+    def create
       # debugger
       @user = User.new(user_params)
       
@@ -14,9 +14,26 @@ class UsersController < ApplicationController
           render 'new'
           
       end
-   end
+    end
    
+    def edit 
+      @user = User.find(params[:id]) 
+    end
    
+    def update
+        @user = User.find(params[:id])
+      
+        if @user.update(user_params)
+            flash[:success] = "Your accout was updated successfully" 
+            redirect_to articles_path
+          
+        else
+            render 'edit'
+          
+        end
+    end
+   
+ 
    private
         def user_params
             params.require(:user).permit(:username, :email, :password)
