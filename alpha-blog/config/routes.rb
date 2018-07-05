b31eac3a061af4f21d8ff3cfeb0505853bc015c0
hikozuma <curoas@yahoo.co.jp>
2018-07-05 03:39:22 +0000

    signup users from UI

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :users, except:[:new]
   
-  root 'pages#home'
-  get 'about', to: 'pages#about'
+  root  'pages#home'
+  get   'about',  to: 'pages#about'
+  
+  get   'signup', to: 'users#new'
+  # post  'users',  to: 'users#create'
 end
